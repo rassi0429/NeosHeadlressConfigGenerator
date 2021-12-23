5783438105654bc6ed76f677f9f35664f88d03fb
rassi0429 <taitokokoa+rassi@gmail.com>
2021-12-23 18:57:20 +0900

    feat: WIP

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  v-app(dark)
+    v-app-bar(fixed app dense)
+      v-toolbar-title NeosVR Headless Config Generator
+    v-main
+      v-container
+        Nuxt
+    v-footer(app)
+      | @rassi0429 &nbsp;
+      a(href="https://github.com/rassi0429/NeosHeadlressConfigGenerator") GitHub Repo
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  container()
+    v-row
+      v-col(cols=7)
+        v-card
+          v-card-title Form
+          v-divider
+          v-card-text
+            v-text-field(outlined dense :label="test" :messages="schema.properties.comment.description")
+      v-col(cols=5)
+        v-card
+          v-card-title Vieweraaa
+          v-card-text
+            | Here is viewer
+</template>
+
+<script>
+import schema from '@/assets/json/schema.json'
+
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      schema,
+      test: "teetet",
+      comment: "",
+      universeId: null,
+      tickRate: 60.0,
+      maxConcurrentAssetTransfers: 6,
+      usernameOverride: null,
+      loginCredential: "",
+      loginPassword: "",
+      startWorlds:[],
+      dataFolder: null,
+      cacheFoler: null,
+      logsFolder: null,
+      allowedUrlHosts: [],
+      autoSpawnItems: [],
+    }
+  }
+}
+</script>
